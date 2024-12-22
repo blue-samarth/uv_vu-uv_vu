1b76d233622a39db94b8a1190abacd2bbf87789f
blue-samarth <samarth38work@gmail.com>
2024-12-23 00:12:12 +0530

    Upgrades in assertion files

--- libs/assertions.py
+++ libs/assertions.py
@@ -26,3 +26,16 @@ def assert_not_none(value , message : str = "Value is None" , status_code : int 
         CustomError : If the assertion fails
     """
     base_assertion(value is not None , message , status_code)
+
+
+def assert_not_found(value , message : str = "Resource not found" , status_code : int = 404) -> None:
+    """
+    This function will assert that the given value is not None
+    Args:
+        value : Any : The value to assert
+        message : str : The message to return if the assertion fails
+        status_code : int : The status code to return if the assertion fails
+    Raises:
+        CustomError : If the assertion fails
+    """
+    base_assertion(value is not None , message , status_code)
